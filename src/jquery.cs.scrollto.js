147dc1f9e571df8cd166a34537d095940d82114a
Chris Slater <github@chris-slater.com>
2013-05-31 19:44:43 +0100

    made fixes to scrollto, now working as expected.

--- src/jquery.cs.scrollto.js
+++ src/jquery.cs.scrollto.js
@@ -1,37 +1,45 @@
+/*
+* Copyright 2013 Christopher Slater (chris-slater.com) and other contributors
+* Released under the MIT license
+* See the file MIT-LICENSE.txt for copying permission. The license can also be found at: http://opensource.org/licenses/MIT
+*/
 (function($){
 
     $.cs = $.cs || {}
 
     $.cs.scrollTo = {
         conf: {
+            selector: 'a[href^="#/"]',
             before: function(){},
             after: function(){}
         }
     };
 
     // Constructor
-    function ScrollTo(this, conf){
-        console.log('meh', this);
-    // Bind Scroll event to links
-        $('a[href^="/#"]').click(function(e){
+    function ScrollTo(root, conf){
+        // Bind Scroll event to links
+
+        $(conf.selector).click(function(e){
             
             e.preventDefault();
-            $this = $(this);
-
+            var $this = $(this);
+            
             var offset = $($this.attr('href').replace('/', '')).offset();
-            $('html,body').animate({ scrollTop: offset.top, scrollLeft: offset.left });
+            
+            if (offset) {
+                $('html,body').animate({ scrollTop: offset.top, scrollLeft: offset.left });
+            }
         });
     }
 
     // jQuery plugin
     $.fn.csScrollTo = function(conf){
-        console.log('this is workign');
 
         var el = this.data('csScrollTo');
         if (el) { return el; }
 
         conf = $.extend({}, $.cs.scrollTo.conf, conf);
 
-        var blah = new ScrollTo(this, conf);
+        var instance = new ScrollTo(this, conf);
     }
 })(jQuery);
